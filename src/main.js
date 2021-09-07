fe4b4487b7c53c074a2f361973d68b4ed32b9211
jyst <yeongsinjoung@gmail.com>
2021-09-08 04:05:14 +0900

    Create JSON data

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+
+// Fetch로 JSON 데이터 받아오기: response-body안에 json이 들어있음
+function loadItems() {
+  return fetch('data/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
+}
+
+
+
+
+// 시간이 걸리기 때문에 promise return
+// main
+loadItems()
+  .then(items => {
+    console.log(items);
+    // dispalyItems(items);
+    // setEventListeners(items);
+  })
+  .catch(console.log("ERROR"));
