f3aec3a97f254fc2fc989c2c554394c46eb1dad7
jyst <yeongsinjoung@gmail.com>
2021-09-08 12:29:54 +0900

    Create main functions

--- src/main.js
+++ src/main.js
@@ -5,18 +5,54 @@
 function loadItems() {
   return fetch('data/data.json')
     .then(response => response.json())
-    .then(json => json.items);
+    .then(data => data.items);
+}
+
+function createHTMLString(item) {
+  return `
+  <li class="item">
+    <img class="item__thumnail" src=${item.src} alt=${item.color}_${item.type}>
+    <span class="item__description">${item.gender}, ${item.size} size</span>
+  </li>
+  `;
+}
+
+function dispalyItems(items) {
+  const container = document.querySelector('.lists');
+  const htmlString = items.map(item => createHTMLString(item)).join('');
+  container.innerHTML = htmlString;
+}
+
+
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if(key == null || value == null) {
+    return;
+  }
+
+  const filteredItems = items.filter(item => item[key] === value);
+  console.log(filteredItems);
+  dispalyItems(filteredItems);
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.shoping_nav');
+
+  logo.addEventListener('click', () => dispalyItems(items));
+  buttons.addEventListener('click', event => onButtonClick(event, items));
 }
 
 
 
-
-// 시간이 걸리기 때문에 promise return
+// 자료를 받아올 때 시간이 걸리기 때문에 promise return
 // main
 loadItems()
   .then(items => {
-    console.log(items);
-    // dispalyItems(items);
-    // setEventListeners(items);
+    dispalyItems(items);
+    setEventListeners(items);
   })
   .catch(console.log("ERROR"));
